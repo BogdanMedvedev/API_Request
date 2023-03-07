c2f948ff1dc70a6bddf32ea7421d7be044f1f2e0
BogdanMedvedev <bgdnmdvdv@gmail.com>
2023-03-07 09:48:33 +0300

    updated code comments

--- Request-POST.py
+++ Request-POST.py
@@ -1,5 +1,5 @@
 # Необходимо выполнить авторизацию с произвольными данными
-# на тестовом сайте http://quotes.toscrape.com с произвольными данными
+# на тестовом сайте http://quotes.toscrape.com
 
 from requests import Session
 from bs4 import BeautifulSoup
@@ -7,26 +7,26 @@ from bs4 import BeautifulSoup
 # Необходимо использовать headers в get и post запросах, чтобы обойти защиту сайтов
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'}
 
-# создаём сессию, чтобы работать в рамках одной сессии, сохраняя файлы cookie
+# Создаём сессию, чтобы работать в рамках одной сессии, сохраняя файлы cookie
 work = Session()
 
-# заходим на сайт
+# Переходим на сайт в рамках сессии
 work.get('https://quotes.toscrape.com', headers=headers)
 
-# заходит на страницу авторизации
+# Переходим на страницу авторизации
 response = work.get('https://quotes.toscrape.com/login', headers=headers).text
 
-# создаём элемент BeautifulSoup, чтобы удобнее делать поиск по странице
+# Создаём элемент BeautifulSoup, чтобы было удобнее выполнять поиск по странице
 soup = BeautifulSoup(response, 'lxml')
 
-# вытаскиваем токен и сохраняем в переменную для дальнейшей авторизации
+# Вытаскиваем из сессии токен и сохраняем в переменную для дальнейшей авторизации
 token = soup.find('input', type='hidden').get('value')
 
-# создаём словарь с данными для авторизации
+# Создаём словарь с данными для последующей авторизации
 data = {'csrf_token': token, 'username': 1234, 'password': 1234}
 
-# проходим авторизацию на сайте, атрибут allow_redirects позволяет разрешить перенаправление
+# Выполняем авторизацию на сайте, атрибут "allow_redirects" позволяет разрешить перенаправление, если оно есть
 result = work.post('https://quotes.toscrape.com/login', headers=headers, data=data, allow_redirects=True)
 
-# убедимся, что вход выполнен, найдя на странице "Logout"
+# Убедимся, что вход выполнен, найдя на странице "Logout" (оно появляется только после атворизации)
 print(BeautifulSoup(result.text, 'lxml').find('div', class_='col-md-4').text.replace('\n',''))
